bd1f97852b49cc36e70415b9990b776d0ba6829a
Yokeer <Alexanderdlack34@gmail.com>
2022-08-13 00:06:32 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function toReadable (number) {
   
+  let a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    let b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let digit = number.toString();
+    if (number < 0) return 'Only positive numbers';
+    if (number === 0) return 'zero';
+    if (number < 20) return a[number];
+    if (digit.length === 2) {
+        if (number % 10 === 0) return b[digit[0]];
+        else return b[digit[0]] + ' ' + a[digit[1]];
+    }
+    if (digit.length === 3) {
+        if (number % 100 === 0) return a[digit[0]] + ' hundred';
+        else {
+            return a[digit[0]] + ' hundred ' + toReadable(+(digit[1]+digit[2]));
+        }
+    }
 }
